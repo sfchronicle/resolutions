e68b077bbd4d7a5ceb8c4900a305fc306b7b0d89
bdin <b.din1996@gmail.com>
2016-08-08 15:41:13 -0700

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,35 @@
+// require("./lib/social");
+
+
+require("component-responsive-frame/child");
+
+var r = [];
+var e = document.getElementById("event");
+var s = document.getElementById("statement");
+
+for ( var i = 0; i < resolutionsData.length; i++ ) {
+	r.push(resolutionsData[i].resolution);
+}
+
+var count = 0;
+function changeText() {
+	  e.style.opacity = 0;
+
+	  var last = +new Date();
+	  var tick = function() {
+	    e.style.opacity = +e.style.opacity + (new Date() - last) / 1000;
+	    last = +new Date();
+
+	    if (e.style.opacity < 1) {
+	      (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+	    }
+	  };
+
+	  tick();
+
+    e.textContent = r[count] + ".";
+    count < resolutionsData.length - 1 ? count++ : count = 0;
+}
+
+changeText();
+setInterval(changeText,5000);
